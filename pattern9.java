63cf04d98f13653e446d4b6ca8fdc220c84ccf70
Muntazir Abbas <abbasmuntazir62@gmail.com>
2026-01-22 12:07:47 +0530

    Add files via upload

---
+++ pattern9.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Pattern9 {
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i = 1; i <= n; i++) {
+            // Prints i columns per row
+            for (int j = 1; j <= i; j++) {
+                if (i+j % 2 == 0) {
+                    System.out.print("1 ");
+                } else {
+                    System.out.print("0 ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
